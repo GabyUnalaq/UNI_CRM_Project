11d876002f707bd844cc9f46ec12d972e14e332e
Tomuta <Gabriel.Tomuta@elektrobit.com>
2020-12-14 21:18:22 +0200

    Updates
     - Added .csv
     - Work on Login

--- src/LoginWindow.py
+++ src/LoginWindow.py
@@ -16,32 +16,65 @@ import csv
 class LoginWindow(QMainWindow):
     def __init__(self):
         super().__init__()
+        self.csv = None
         uic.loadUi(r"..\ui\LoginInterface.ui", self)
-        self.setWindowTitle("Send E-mail")
+        self.setWindowTitle("Login into CRM")
 
         # Widgets
         self.Qmain_login = self.findChild(QWidget, "Qmain_login")
         self.Qbutton_login = self.findChild(QPushButton, "Qbutton_login")
-        self.Qtext_email = self.findChild(QPlainTextEdit, "Qtext_email")
-        self.Qtext_password = self.findChild(QPlainTextEdit, "Qtext_password")
+        self.Qbutton_login.setCheckable(True)
+        self.Qtext_email = self.findChild(QLineEdit, "Qtext_email")
+        self.Qtext_password = self.findChild(QLineEdit, "Qtext_password")
+
+        self.Qtext_email.setPlaceholderText('Introduceti e-mail')
+        self.Qtext_password.setPlaceholderText('Introduceti parola')
+        self.Qtext_password.setEchoMode(QLineEdit.Password)
 
         # Members
         # TODO:
 
         # Signals
-        # TODO:
+        self.Qbutton_login.clicked.connect(self.check_password)
+        # TODO: Horsexiu
 
         # Init
         # TODO:
 
-    # Initialization ---------------------------------------------------------------------------------------------------
-    # TODO:
+    def initialization(self):
+        self.read_csv()
+        print(self.csv)
+        # TODO
 
     # Methods ----------------------------------------------------------------------------------------------------------
     # TODO:
 
     # Slots ------------------------------------------------------------------------------------------------------------
     # TODO:
+
+    def read_csv(self):
+        with open('../data/login_info.csv') as csv_file:
+            self.csv = csv.reader(csv_file, delimiter=',')
+
+    def check_password(self):
+        msg = QMessageBox()
+        msg.setIcon(QMessageBox.Information)
+        msg.setStandardButtons(QMessageBox.Ok)
+
+        if self.Qtext_email.text() == 'Farkas' and self.Qtext_password.text() == 'prost34':
+            msg.setWindowTitle("Locare reusita")
+            msg.setText('Success')
+            msg.exec_()
+            self.redirect_to_main()
+            app.quit()
+        else:
+            msg.setWindowTitle("Eroare")
+            msg.setText('E-mail sau parola incorecta')
+            msg.exec_()
+
+    def redirect_to_main(self):
+        pass
+        # TODO
 
 
 def show_window():
